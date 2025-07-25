a2dba6fe1103ff7bf42935eeb7edff7ea028985e
Gildácio Lopes <gildacioj399@gmail.com>
2025-07-25 04:24:57 -0300

    chore: frontend setup, vite, tailwind and shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Hello</h1>
+      <Button variant={"outline"}>Click me plis</Button>
+    </>
+  );
+}
+
+export default App;
