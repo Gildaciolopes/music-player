1d4622fdd68ec609e82cc4c97c013c2175ad4635
Gildácio Lopes <gildacioj399@gmail.com>
2025-09-30 14:04:04 -0300

    feat: add Topbar component for navigation and user authentication

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,26 @@
+import { SignedOut } from "@clerk/clerk-react";
+import { LayoutDashboardIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Topbar = () => {
+  const idAdmin = false;
+  return (
+    <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
+      <div className="flex gap-2 items-center">Fresh Music</div>
+      <div className="flex items-center gap-4">
+        {idAdmin && (
+          <Link to={"/admin"}>
+            <LayoutDashboardIcon className="size-4 mr-2" />
+            Admin Dashboard
+          </Link>
+        )}
+
+        <SignedOut>
+          <SignInOAuthButtons />
+        </SignedOut>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
