93cccd475cbfcb329c4778ddad5c84de3146dd7a
Gildácio Lopes <gildacioj399@gmail.com>
2025-10-01 05:59:05 -0300

    feat: add SSO callback route with authentication redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signInForceRedirectUrl={"/auth-callback"}
+            />
+          }
+        ></Route>
         <Route path="/auth-callback" element={<AuthCallbackPage />}></Route>
       </Routes>
     </>
